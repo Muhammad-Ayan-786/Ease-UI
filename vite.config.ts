2efe19c4628c22c67ab78aea37ef055feefdc785
Muhammad Ayan <mohd.ayanasim.786@gmail.com>
2026-08-22 17:01:54 +0200

    .

--- vite.config.ts
+++ vite.config.ts
@@ -5,26 +5,41 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
-      name: "EaseUI",
-      fileName: (format) => `easeui.${format}.js`,
-    },
-    cssCodeSplit: true,
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
-      },
+      "@": path.resolve(process.cwd(), "./src"),
     },
   },
 });
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+// import path from "path";
+
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+//   build: {
+//     lib: {
+//       entry: path.resolve(__dirname, "src/index.ts"),
+//       name: "EaseUI",
+//       fileName: (format) => `easeui.${format}.js`,
+//     },
+//     cssCodeSplit: true,
+//     rollupOptions: {
+//       external: ["react", "react-dom"],
+//       output: {
+//         globals: {
+//           react: "React",
+//           "react-dom": "ReactDOM",
+//         },
+//       },
+//     },
+//   },
+// });
